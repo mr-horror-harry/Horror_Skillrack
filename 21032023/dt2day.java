521b7ef354299eadcac4811afdded8756410aa6a
M A Harish J <mr.horror3737@gmail.com>
2023-03-23 04:19:49 +0530

    2203

---
+++ 21032023/dt2day.java
@@ -0,0 +1,16 @@
+
+import java.util.*;
+
+class Hello {
+    public static void main(String args[]) {
+        int n = new Scanner(System.in).nextInt();
+        StringBuilder sb = new StringBuilder("");
+
+        while (n > 0) {
+            sb.append(n % 8);
+            n /= 8;
+        }
+
+        System.out.print(sb.reverse().equals(sb) ? "YES" : "NO");
+    }
+}
