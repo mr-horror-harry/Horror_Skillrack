cae41d17f3aca26040495bd5fceee782984fbb3c
M A Harish J <103276323+mr-horror-harry@users.noreply.github.com>
2023-03-23 04:26:18 +0530

    Update dt2day.java

--- 21032023/dt2day.java
+++ 21032023/dt2day.java
@@ -4,7 +4,7 @@ import java.util.*;
 class Hello {
     public static void main(String args[]) {
         int n = new Scanner(System.in).nextInt();
-        StringBuilder sb = new StringBuilder("");
+        StringBuilder sb = new StringBuilder("");   // ignore the quotespace in the stringbuilder in photo
 
         while (n > 0) {
             sb.append(n % 8);
