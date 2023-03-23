558160d66c2e0b6232b56c795e3337e4f95a2434
M A Harish J <mr.horror3737@gmail.com>
2023-03-23 13:23:07 +0530

    23 commit

---
+++ 23032023/SolutionDC.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+class Hello {
+    public static void main(String[] args){
+        
+        Scanner sc = new Scanner(System.in);
+        
+        short n = sc.nextShort();
+        int ar[]  = new int[n];
+        
+        short i=0;
+        while(--n>=0){
+            ar[n]=sc.nextInt();
+            i++;   
+        }
+        
+        short x = sc.nextShort();
+        
+        while(i-->0) System.out.print(ar[i]%2==0 ? ar[i]+x +" " : ar[i]-x +" ");
+        
+	}
+}
