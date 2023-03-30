762ead5c397df4a105716847823a9a378cb56afc
M A Harish J <mr.horror3737@gmail.com>
2023-03-30 16:16:05 +0530

    java dc soln 30

---
+++ 30032023/SolutionDC.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+class Main
+{
+	public static void main(String[] args) {
+	    Soln soln = new Soln( new Scanner(System.in).next());
+	    soln.stringParse();
+	    
+	    int res = soln.res;
+	    
+	    System.out.print(res==0 ? -1 : res>0 ? "FIRSTHALF" : "SECONDHALF");
+	    return;
+	}
+}
+
+interface SolnInterface{
+    public void stringParse();
+    public void simple(); 
+}
+
+class Soln implements SolnInterface{
+    int res=0;
+    String s;
+    
+    Soln(String str){
+        s=str;
+    }
+    
+    public void stringParse(){
+        for(char c : s.toCharArray()){
+            res += c>='a' && c<='m' ? 1 : -1;
+        }
+    }
+    public void simple(){
+        
+    }
+}
