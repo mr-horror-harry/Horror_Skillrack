7126a83e3f5cf1ea10a1670e3806f3938d45cf48
M A Harish J <mr.horror3737@gmail.com>
2023-04-06 14:41:20 +0530

    dc java Eski cmmt

---
+++ 06042023/SolutionDC.java
@@ -0,0 +1,34 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main()
+{
+    int n;
+    scanf("%d ",&n);
+    
+    int val=1, dash=0, r=1, beg, end, inc;
+    while(n>0){
+        for(int i=0; i<dash; i++) printf("- ");
+        dash++;
+        
+        int lim=val;
+        if(r%2==1){
+            for(int i=lim; i<lim+n; i++){
+                printf("%d ", i);
+                printf(i<lim+n-1 ? "* " : "");
+                val++;
+            }
+        }else{
+            for(int i=lim+n-1; i>=lim; i--){
+                printf("%d ",i);
+                printf(i>lim ? "* " : "");
+                val++;
+            }
+            
+        }
+        printf("\n");
+        r++;
+        n--;
+    }
+
+}
