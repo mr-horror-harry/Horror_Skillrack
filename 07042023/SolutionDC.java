ca7e85589c372eb82f488c9eff2a88866e2c090b
M A Harish J <mr.horror3737@gmail.com>
2023-04-07 23:57:48 +0530

    dc jv

--- 07042023/SolutionDC.java
+++ 07042023/SolutionDC.java
@@ -1,9 +1,19 @@
 import java.util.Scanner;
 
-public class Main
+class Main
 {
-	public static void main(String[] args) {
-	    
-
+	public static void main(String[] args){
+		String s = new Scanner(System.in).next();
+		
+		for(int i=0; i<s.length(); i+=2){
+			if(i==0){
+				if(s.charAt(1)!='0') System.out.print(s.charAt(1));
+				System.out.print(s.charAt(0));
+			}
+			else{
+				System.out.print(s.charAt(i+1) +""+ s.charAt(i));
+			}
+		}
+		
 	}
 }
