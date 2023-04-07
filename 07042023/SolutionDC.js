096504a7d3dd176ae030bf36bda007e449a35872
M A Harish J <mr.horror3737@gmail.com>
2023-04-07 13:28:25 +0530

    dc js updt

---
+++ 07042023/SolutionDC.js
@@ -0,0 +1,24 @@
+var readline = require('readline');
+var reader = readline.createInterface({
+  input: process.stdin,
+  terminal: true
+});
+
+var lines = [];
+
+reader.on('line', function (line) {
+    lines.push(line);
+});
+
+reader.on('close', function () {
+    
+    var ar = lines[0].split("");
+    
+    var res=0;
+    for(i=0; i < ar.length; i+=2){
+        res = (res*10) + parseInt(ar[i+1]);
+        res = (res*10) + parseInt(ar[i]);
+    }
+    console.log(res);
+    
+});
