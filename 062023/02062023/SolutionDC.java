4b65dbc038f9c767347133b796ab4d3658eb81eb
M A Harish J <mr.horror3737@gmail.com>
2023-06-02 11:23:04 +0530

    dc push

---
+++ 062023/02062023/SolutionDC.java
@@ -0,0 +1,26 @@
+import java.util.*;
+public class Hello {
+
+    public static void main(String[] args) {
+        
+        byte n=new Scanner(System.in).nextByte();
+        byte d=(byte)(n-1), tmp=0;
+        
+        int ct=2*n-1, val=0, vCt=1, tCt;
+        while(ct-->0){
+            
+            System.out.print("-".repeat(d)+"<");
+            
+            tCt=vCt;
+            while(tCt-->0){
+                System.out.print(val++ %10);
+            }
+            System.out.println(">");
+            
+            tmp = ct>=n ? (byte)-1 : (byte)+1;
+            d+=tmp;
+            vCt+= -2*tmp;
+            
+        }
+	}
+}
