a0e5d34b6d102df914d293b7f274da7b95fd168e
M A HARISH J <mr.horror3737@gmail.com>
2023-07-18 09:51:55 +0530

    dc

---
+++ 072023/18072023/SolutionDC.java
@@ -0,0 +1,32 @@
+import java.util.*;
+public class Hello {
+    
+    static boolean isCons(char c){
+        return !(c=='a' || c=='e' || c=='i' || c=='o' || c=='u');
+    }
+
+    public static void main(String[] args) {
+        
+        char ar[] = new Scanner(System.in).next().toCharArray();
+        int hash[] = new int[26];
+        
+        for(char c:ar)
+            if(isCons(c))
+                hash[c-'a']++;
+        
+        int res=0, oF=0;
+        for(int i:hash){
+            if(i%2==0)
+                res+=i;
+            else{
+                oF=1;
+                if(i>2){
+                    res+=i-1;
+                }
+            }
+        }
+        
+        System.out.print(oF==1 ? ++res : res);
+
+	}
+}
