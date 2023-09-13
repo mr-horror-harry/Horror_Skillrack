0e5c0b09793ece4be063dccfe6497d91d7919b8c
M A HARISH J <mr.horror3737@gmail.com>
2023-09-13 21:18:53 +0530

    dc2

---
+++ 092023/13092023/SolutionDC2.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class Hello {
+
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+        
+        while(sc.hasNext())
+            System.out.print(-1*sc.nextInt() + " ");
+            
+	}
+}
