4e82e12f573f547dc106d67fcd9c2c974c45a270
M A HARISH J <mr.horror3737@gmail.com>
2023-10-09 16:44:32 +0530

    dt

--- 102023/01102023/SolutionDC.java
+++ 102023/01102023/SolutionDC.java
@@ -1,5 +1,5 @@
 import java.util.*;
-public class Hello {
+class Hello {
 
     public static void main(String[] args) {
         
---
+++ 102023/09102023/SolnDT.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class Hello {
+
+    public static void main(String[] args) {
+        
+        String n = new Scanner(System.in).next();
+        
+        System.out.print(Integer.toBinaryString(Integer.parseInt(n, 8)));
+        
+	}
+}
