27e602acd1642abfdea9134989235f93bcb13322
M A HARISH J <mr.horror3737@gmail.com>
2023-10-18 19:50:32 +0530

    dt

---
+++ 102023/18102023/SolnDC.py
@@ -0,0 +1,16 @@
+s=input()
+vow='aeiouAEIOU'
+f=""
+pos=""
+
+for i in range(len(s)-1, -1, -1):
+	if s[i] in vow:
+		if f!="":
+			s[pos]=s[i]
+			pos=i
+		else:
+			f=s[i]
+			pos=i
+
+for c in s:
+	print(c, end="")
